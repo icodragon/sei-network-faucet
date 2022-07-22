2ea049d737bc651ec1b8909cc943b7536963fcd8
Dragon <icodragon@ivanich.ospd.net>
2022-07-23 06:28:27 +1000

    add try catch

--- server/controllers/api/claimTokens.js
+++ server/controllers/api/claimTokens.js
@@ -34,12 +34,16 @@ module.exports = async (req, res, next) => {
 };
 
 async function claimToken(address) {
-    const cmd = await exec(`seid tx bank send sei1yv0cjc33480q8kt2ngnpkm090ylls9aqmfneme ${address} 100usei --chain-id=atlantic-1 --node=tcp://88.99.104.186:11301 -y`);
-    const data = cmd.stdout.toString().split('\n');
-    for (let line of data) {
-        if (line.includes('txhash:')) {
-             return line;
-         }
+    try {
+        const cmd = await exec(`seid tx bank send sei1yv0cjc33480q8kt2ngnpkm090ylls9aqmfneme ${address} 100usei --chain-id=atlantic-1 --node=tcp://88.99.104.186:11301 -y`);
+        const data = cmd.stdout.toString().split('\n');
+        for (let line of data) {
+            if (line.includes('txhash:')) {
+                 return line;
+             }
+        }
+    } catch (e) {
+        return 'None';
     }
     return 'None';
 }
